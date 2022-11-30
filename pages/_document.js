5ac922c9f84e51b08a2cfeacbaa2b304d34d1390
Jon Goldson <forjrg@gmail.com>
2022-11-30 13:35:08 -0700

    editing the cover image

--- pages/_document.js
+++ pages/_document.js
@@ -4,15 +4,15 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="GPT-3 Writer" key="title" />
+        <meta property="og:title" content="Daily Workout" key="title" />
         <meta
           property="og:description"
-          content="build with buildspace"
+          content="Custom AI Workouts"
           key="description"
         />
         <meta
           property="og:image"
-          content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
+          content="https://i.ibb.co/q5n2Hjx/dailyworkout.jpg"
         />
         <meta name="twitter:card" content="summary_large_image"></meta>
         <link
